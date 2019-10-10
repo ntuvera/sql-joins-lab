bc358d952001b7c81ba23e0c5eb98c60a1b1db99
ntuvera <ntuvera@gmail.com>
2019-10-10 15:46:16 -0400

    WIP: create bash scripts to setup and initialize tables

--- init_db.sh
+++ init_db.sh
@@ -1,5 +1,29 @@
 #!/bin/sh
 
 # TODO: Create database and initialize database table structure
+database="company"
+
+psql -c 'CREATE DATABASE company'
+# CREATE DATABASE company;
+
+# psql DROP DATABASE $company;
+
+psql -d $database -c "CREATE TABLE employees (
+	ssn PRIMARY KEY NOT NULL,
+	first_name varchar(50),
+	last_name varchar(50),
+	year_of_birth int,
+	city varchar(50)
+)";
+
+psql -d $database -c "CREATE TABLE jobs (
+  FOREIGN KEY (snn) REFERENCES epmloyees(ssn),
+	company varchar(50),
+	salary int,
+	experience int
+)";
 
 # TODO: Insert data into tables
+
+psql -d $database -c "COPY employees(ssn,first_name,last_name,year_of_birth,city) FROM '`pwd`/employees.csv' DELIMITER ',' CSV HEADER";
+psql -d $database -c "COPY jobs(ssn, company, salary, experience) FROM '`pwd`/jobs.csv' DELIMITER ',' CSV HEADER";
--- run_queries.sh
+++ run_queries.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+database="company"
 # TODO: Write queries to display:
 # Employees working at macys
-# Companies in Boston
+psql -d $database -c " SELECT * FROM employees JOIN ON jobs.ssn=employees.ssn WHERE jobs.company LIKE 'Macy%'";
+# Companies in k
+psql -d $database -c "SELECT company FROM jobs JOIN ON jobs.ssn=employee.ssn WHERE employee.city='Boston';"
 # Employee with the highest salary
+psql -d $database -c "SELECT name FROM employees JOIN ON jobs.ssn=employee.ssn ORDER BY salary DESC LIMIT 1";
